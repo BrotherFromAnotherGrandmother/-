a60951b86546895b02bcec625c568fab3e133f8c
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-31 12:25:34 +0300

    Отфильтровал базу данных

--- main.py
+++ main.py
@@ -8,9 +8,5 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    active_passcards = []
-    for passcard in Passcard.objects.all():
-        if passcard.is_active == True:
-            active_passcards.append(passcard)
-    print('Активных пропусков', len(active_passcards))
+    print('Активных пропусков', len(Passcard.objects.filter(is_active=True)))
     print('Всего пропусков', Passcard.objects.count())  # noqa: T001
