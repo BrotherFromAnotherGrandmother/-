b1883563a0709e5451b20d8aa41e0bf3dc5e8e65
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-31 12:03:21 +0300

    Нашёл все действующие пропуска

--- main.py
+++ main.py
@@ -8,8 +8,9 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    print(f'owner_name: {Passcard.objects.all()[8].owner_name}')
-    print(f'passcode: {Passcard.objects.all()[8].passcode}')
-    print(f'created_at: {Passcard.objects.all()[8].created_at}')
-    print(f'is_active: {Passcard.objects.all()[8].is_active}')
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    active_passcards = []
+    for passcard in Passcard.objects.all():
+        if passcard.is_active == True:
+            active_passcards.append(passcard)
+    print('Активных пропусков', len(active_passcards))
+    print('Всего пропусков', Passcard.objects.count())  # noqa: T001
