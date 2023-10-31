80d98e81af629178c1606c769111b99004e0ea7a
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-31 14:15:44 +0300

    Вывел сколько времени посетители провели в хранилище

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import os
 
 import django
+from django.utils.timezone import localtime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard, Visit  # noqa: E402
 
 if __name__ == '__main__':
     print('Активных пропусков', len(Passcard.objects.filter(is_active=True)))
     print('Всего пропусков', Passcard.objects.count())  # noqa: T001
+    visiters_now = Visit.objects.filter(leaved_at=None)  # люди которые не ушли из хранилища
+    for visiter in visiters_now:
+        print(visiter.passcard.owner_name)
+        print(f'Зашёл в хранилище, время по Москве:\n{localtime(visiter.entered_at)}')
+        print(f'Находится в хранилище:\n{localtime() - localtime(visiter.entered_at)}\n')
