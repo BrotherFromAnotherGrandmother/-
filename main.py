870abae74231c2fb76627892fe27cf9634dfacda
BrotherFromAnotherMother <aleksandr.nidodin@gmail.com>
2023-10-31 11:55:23 +0300

    Выбрал один пропуск

--- main.py
+++ main.py
@@ -8,5 +8,8 @@ django.setup()
 from datacenter.models import Passcard  # noqa: E402
 
 if __name__ == '__main__':
-    print(Passcard.objects.all())
+    print(f'owner_name: {Passcard.objects.all()[8].owner_name}')
+    print(f'passcode: {Passcard.objects.all()[8].passcode}')
+    print(f'created_at: {Passcard.objects.all()[8].created_at}')
+    print(f'is_active: {Passcard.objects.all()[8].is_active}')
     print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
